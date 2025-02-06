b4d6098cde9ce6c5c414033038a423ec6f8ca6bd
Sbammm <seba-96@hotmail.it>
2025-02-06 09:11:45 +0100

    esempio_01.py

--- esempio_01.py
+++ esempio_01.py
@@ -1,10 +1,12 @@
-
 cont=0
-while(cont<2):
+valore_precedente=-1
+while(cont<1):
     
     valore= float(input("qui prendo l'acquisizione del numero :"))
-    if valore==0:
+    if valore==0 and valore_precedente==0:
         cont=cont+1
+    valore_precedente=valore
+    
     print("il valore inserito è :", valore)
 
 print("la sequenza di numeri è finita")
