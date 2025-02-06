0e3a7910af44f5ce8f8ea015ab28c57b590cf712
Sbammm <seba-96@hotmail.it>
2025-02-06 09:24:24 +0100

    esempio_01.py

--- esempio_01.py
+++ esempio_01.py
@@ -1,12 +1,16 @@
 cont=0
 valore_precedente=-1
+somma=0
 while(cont<1):
     
     valore= float(input("qui prendo l'acquisizione del numero :"))
+
     if valore==0 and valore_precedente==0:
         cont=cont+1
     valore_precedente=valore
+    somma+=valore
     
     print("il valore inserito è :", valore)
 
 print("la sequenza di numeri è finita")
+print("la somma è :", somma)
