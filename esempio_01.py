15db4ae3e49e9afaa7cf2457b2659133ceb6c6ad
Sbammm <seba-96@hotmail.it>
2025-02-06 09:03:52 +0100

    Add files via upload

---
+++ esempio_01.py
@@ -0,0 +1,10 @@
+
+cont=0
+while(cont<2):
+    
+    valore= float(input("qui prendo l'acquisizione del numero :"))
+    if valore==0:
+        cont=cont+1
+    print("il valore inserito è :", valore)
+
+print("la sequenza di numeri è finita")
