92f431eb12bb7757445ee605ce325314ca39044c
Sbammm <seba-96@hotmail.it>
2025-02-09 20:14:07 +0100

    esempio_03.py

--- esempio_03.py
+++ esempio_03.py
@@ -1,42 +1,66 @@
-partita = True
+
 giocatore1=0
 giocatore2=0
-punto1=0
-punto2=0
+valutazione_giocatore=0
+vincitore=-1
+finale=True
+punto_vittoria1=0
+punto_vittoria2=0
+print("funziona?!?!?!??!")
 
-while partita:
-    inserimento=int(input("inserire il numero del giocatore che ha fatto il punto : "))
-
-    if inserimento==1 :
-
-        if giocatore1<30 :
+while finale==True:
+   
+    valutazione_giocatore= int(input("Inserire chi ha vinto il set con 1 o 2: ")) 
+    
+    #pezzo di codice che assegna il punteggio ad il primo giocatore
+    if valutazione_giocatore==1:
+        if giocatore1<30:
             giocatore1+=15
 
-        else :
+        elif giocatore1<40:
             giocatore1+=10
-            punto1+=1
-            if punto1>=2:
-                giocatore1=giocatore1-10
-                partita=False
-                print("HA VINTO IL GIOCATORE 1")
-        
-    else:
-
-        if giocatore2< 30:
-            giocatore2+=15
-
+            punto_vittoria1+=1
         else:
-            giocatore2+=10
-            punto1+=1
-            if punto2>=2:
-                giocatore2=giocatore2-10
-                partita=False
-                print("HA VINTO IL GIOCATORE 2")
-
-
-    #print("il punteggio del giocatore 1 è : ", giocatore1)
-    #print("il punteggio del giocatore 2 è : ", giocatore2)
+            punto_vittoria1+=1
 
     
+
+
+    #pezzo di codice che assegna il punteggio ad il secondo giocatore
+    if valutazione_giocatore==2:
+        if giocatore2<30:
+            giocatore2+=15
+
+        elif giocatore2<40:
+            giocatore2+=10
+            punto_vittoria2+=1
+        else:
+            punto_vittoria2+=1
+
+
+
+
+#pezzo di codice che mi controlla dai 40 punti il primo che fa due vittorie di fila sull avversario
+    if punto_vittoria1==1 and punto_vittoria2==1:
+        punto_vittoria1=0
+        punto_vittoria2=0
+    elif punto_vittoria1==2:
+        vincitore=1
+        finale=False
+        print("il vincitore della competizione è il giocatore 1")
+    elif punto_vittoria2==2:
+        vincitore=2
+        finale=False
+        print("il vincitore della competizione è il giocatore 2")
+
+    print("IL PUNTEGGIO 1: ",giocatore1)    
+    print("il punteggio 2: ", giocatore2)
+#print("punto vittoria 1 :",punto_vittoria1)
+#print("PUNTO VITTORIA2: ", punto_vittoria2)
+print("VINCITOREEEEE",vincitore)
+
+    
+
+        
 
     
